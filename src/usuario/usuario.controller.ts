633d16548908593c7f96e83d18c338667d351142
Erik <erikgazetagois@hotmail.com>
2025-03-11 21:50:53 -0300

    novos conteudos POO

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { UsuariosArmazenados } from "./usuario.dm";
 import { UsuarioEntity } from "./usuario.entity";
 import { criaUsuarioDTO } from "./dto/usuario.dto";
 
 import {v4 as uuid} from 'uuid';// importante que seja colocado o import dessa forma sempre
 import { ListaUsuarioDTO } from "./dto/consulta.dto";
+import { alteraUsuarioDTO } from "./dto/alteraUsuario.dto";
+
 
 @Controller('/usuarios')
 export class UsuarioController{
@@ -43,4 +45,31 @@ export class UsuarioController{
         return listaRetorno;
     }
     
+    @Put('/:id')
+    async atualizaUsuario(@Param('id') id: string, @Body() novosDados: alteraUsuarioDTO){
+        const usuarioAtualizado = await this.clsUsuariosArmazenados.atualizaUsuario(id, novosDados);
+        return {
+            usuario: usuarioAtualizado,
+            message: "Usuario Atualizado"
+    }
 }
+
+@Delete('/:id')
+async removeUsuario(@Param('id') id: string){
+    const usuarioRemovido = await this.clsUsuariosArmazenados.removeUsuario(id);
+    return {
+        usuario: usuarioRemovido,
+        message: "Usuario Removido"
+    }
+}
+
+}
+
+/* {
+    "nome":"Erik Gazeta",
+    "Idade": 29,
+    "cidade":"Bauru",
+    "Email": "erikgazetagois@hotmail.com",
+    "Telefone":"996858619",
+    "Senha": "123456789"
+} */
--- src/usuario/usuario.dm.ts
+++ src/usuario/usuario.dm.ts
@@ -13,6 +13,44 @@ export class UsuariosArmazenados{
         return this.#usuarios;
     }
 
+    atualizaUsuario(id: string, dadosAtualizacao: Partial<UsuarioEntity>){
+        const usuario = this.buscaPorID(id);
+        Object.entries(dadosAtualizacao).forEach(
+            ([chave, valor]) => {
+                if(chave === 'id'){
+                    return
+                }
+                if (valor !== undefined){
+                    return}
+                    
+                usuario[chave] = valor;
+            }
+        )
+        return usuario;
+    }
+
+    private buscaPorID(id: string){
+        const possivelUsuario = this.#usuarios.find(
+            usuarioSalvo => usuarioSalvo.id === id
+        )
+        if(!possivelUsuario){
+            throw new Error('Usuário não encontrado');
+        }
+    
+
+    // Object.assign(usuario, dadosAtualizacao);
+        return possivelUsuario;
+    }
+
+    async removeUsuario(id: string){
+        const usuario = this.buscaPorID(id);
+
+        this.#usuarios = this.#usuarios.filter(
+            usuarioSalvo => usuarioSalvo.id !== id);
+            return usuario;
+    }
+
+
     async validaEmail(email: string): Promise<boolean>{
         const possivelUsuario = this.#usuarios.find(
             usuario => usuario.email === email
